9a577ba8252f63e024b7750f0fdb7f0834d9274e
Bruno Mota <bmotadev@gmail.com>
2023-02-16 12:00:25 -0300

    método construtor em dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:objetos/src/pessoa.dart';
 
 void main(List<String> arguments) {
   // variavel pessoa do tipo Pessoa e recebe o objeto Pessoa
-  Pessoa pessoa  = Pessoa(); // objeto instanciado
+  Pessoa pessoa  = Pessoa("Bruno", 29, "m"); // objeto instanciado
   print(pessoa.nome);
   print(pessoa.idade);
   print(pessoa.sexo);
--- lib/src/pessoa.dart
+++ lib/src/pessoa.dart
@@ -2,4 +2,11 @@ class Pessoa {
   String nome = "Bruno";
   int idade = 29;
   String sexo = "M";
+
+  // construtor tem o mesmo nome da classe
+  Pessoa(String nome, int idade, String sexo){ // método construtor faz a inicialização da classe, construtor é a forma de construir a classe dinamicamente
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+  }
 }
