d0ac28b7270ee66700f4182fa691a43e7eb939e1
Bruno Mota <bmotadev@gmail.com>
2023-02-16 12:10:24 -0300

    construtores inteligentes

--- lib/src/pessoa.dart
+++ lib/src/pessoa.dart
@@ -3,10 +3,12 @@ class Pessoa {
   int idade = 29;
   String sexo = "M";
 
-  // construtor tem o mesmo nome da classe
-  Pessoa({String? nome, int? idade, String? sexo}){ // método construtor faz a inicialização da classe, construtor é a forma de construir a classe dinamicamente
-    this.nome = nome!;
-    this.idade = idade!;
-    this.sexo = sexo!;
-  }
+  Pessoa({required this.nome, required this.idade, required this.sexo}); // construtor
+
+  // // construtor tem o mesmo nome da classe
+  // Pessoa({String? nome, int? idade, String? sexo}){ // método construtor faz a inicialização da classe, construtor é a forma de construir a classe dinamicamente
+  //   this.nome = nome!; // o this mostra que podemos recuperar a variavel de forma global
+  //   this.idade = idade!;
+  //   this.sexo = sexo!;
+  // }
 }
